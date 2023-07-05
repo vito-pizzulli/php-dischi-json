a58cd57f3c81cfebe5eefa6bf8fb7ad4796b9ac9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-05 14:29:59 +0200

    init index.php, imported axios & Vue 3 via CDN

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+
+    <!-- Axios CDN Import-->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    
+    <!-- Vue 3 CDN Import-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+<body>
+
+    <div id="app">
+        
+    </div>
+
+    <script src="./script/main.js"></script>
+</body>
+</html>
