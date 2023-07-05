bab8d10f59e5ac416ad25537f8a1569741b336c7
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-05 16:44:43 +0200

    each disk informations dynamically printed in separate cards

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dischi JSON</title>
+    <link rel="stylesheet" href="./styles/style.css">
 
     <!-- Axios CDN Import-->
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
@@ -14,9 +15,22 @@
 <body>
 
     <div id="app">
-        <ul>
-            <li v-for="(disk, index) in disks" :key="index"> {{ disk.title }}</li>
-        </ul>
+        <header>
+            <div class="container">
+                <img src="./img/spotify-logo.png" alt="Spotify Logo">
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="card" v-for="(disk, index) in disks" :key="index">
+                    <img :src="disk.poster" alt="Disk Poster Image">
+                    <h2>{{ disk.title }}</h2>
+                    <span>{{ disk.author }}</span>
+                    <span>{{ disk.year }}</span>
+                    <span>{{ disk.genre }}</span>
+                </div>
+            </div>
+        </main>
     </div>
 
     <script src="./script/main.js"></script>
