5c21404b94123551b8cdd475a6811aeeda8bf7e1
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-05 16:07:18 +0200

    added created with axios call to get the data array from server.php

--- script/main.js
+++ script/main.js
@@ -1,7 +1,19 @@
 const vueApp = Vue.createApp({
+
     data() {
         return {
-            prova: 'Funziona tutto'
+            apiUrl: './server.php'
         }
+    },
+
+    created() {
+        axios.get(this.apiUrl)
+        .then(response => {
+            console.log(response);
+            this.disks = response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 }).mount('#app');
